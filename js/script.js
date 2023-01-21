97cde93fc23e460b675a904ef0640a717d256c30
amir89roch <123252232+amir89roch@users.noreply.github.com>
2023-01-21 15:40:47 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const menu = document.getElementById("menu");
+const action = document.getElementById("action");
+
+menu.addEventListener("click",()=>{
+    hundelmenu();
+
+})
+
+function hundelmenu(){
+menu.classList.toggle("is-active");
+action.classList.toggle("is-active");
+
+}
